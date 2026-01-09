a5f1b972cdb8cc34641660810c25ec9d07a2a584
Talha Sajid <rts849@outlook.com>
2026-01-09 08:34:46 +0500

    Refactor server startup and add Vercel configuration

--- backend/server.js
+++ backend/server.js
@@ -67,9 +67,11 @@ require("./jobs/cleanup.job");
 // ------------ SERVER + DB ------------
 (async function startServer() {
   await connectDB();
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`✅ Server running on http://localhost:${PORT}`);
-  });
+  // const PORT = process.env.PORT || 5000;
+  // app.listen(PORT, () => {
+  //   console.log(`✅ Server running on http://localhost:${PORT}`);
+  // });
 })();
+
+module.exports = app;
 
